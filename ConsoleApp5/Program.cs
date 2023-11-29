ec1ad80a3ec95cdbd7444e5ec8c0f182df8173f0
huseyn1951 <sultanlkhuseyn@gmail.com>
2023-11-29 17:12:07 +0100

    lesson 7

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////////////////
+            /*
+             Console.Write("Adivi yaz: ");
+             string ad = Console.ReadLine();
+             Console.WriteLine("ad:" + ad);
+
+             Console.Write("Soyadivi yaz: ");
+             string soyad = Console.ReadLine();
+             Console.WriteLine("Soyad: " + soyad);
+
+                                                                    // TASK3
+
+             if (ad.Length>2 && soyad.Length>2)
+             {
+              Console.WriteLine("tam adin: " + ad + " " + soyad);
+             }
+
+             else
+             {
+                 Console.WriteLine("sihtir");
+
+             }
+            */
+            ///////////////////////
+
+
+
+            ///////////////////
+            /*int num1 = 129;
+            int num2 = 11;
+                                                                                //task 4
+            if (num1 == 30 || num2 == 30 || num1 + num2 == 30)
+            {
+                Console.WriteLine(true);
+
+            }
+            else 
+            {
+                Console.WriteLine(false);
+            }*/
+            ///////////////////
+
+            ///////////////////
+
+            /* int temp = 15;
+
+             if (temp < 15)
+             {
+                 Console.WriteLine("SOYUQDU");
+             }
+
+             else if (temp > 15 && temp < 25)                    //TASK5
+             {
+                 Console.WriteLine("hAVA ZORDU");
+             }
+
+             else if (temp > 25)
+             {
+                 Console.WriteLine("hava istidi");
+             }
+             else
+             {
+                 Console.WriteLine("hava 15 qradusdu");
+             }*/
+
+            //////////////////
+
+            ///////////////////////
+
+            /* int a = 9;
+             int b = 21;
+
+             if (a == 30 || b == 30 || a + b == 30)
+             {
+                 Console.WriteLine("true");
+                                                            //task 6
+             }
+
+             else 
+             {
+                 Console.WriteLine("false");
+             }*/
+            /////////////////////////////
+
+            /*Console.WriteLine("imaret tayfa necenci ilde yaranib: ");
+            Console.WriteLine("a)1951");
+            Console.WriteLine("b)2009");
+            Console.WriteLine("c)2004");
+
+            string cavab = Console.ReadLine();
+
+            if (cavab == "b" )
+            {
+                Console.WriteLine("duzdu");
+            }
+
+            else
+            {
+                Console.WriteLine("sehvdir");
+            }
+
+            Console.ReadLine();
+
+
+            
+
+            Console.WriteLine("boshlugu tamamlayin:EN BOYUK IMARET,EN BOYUK ...");
+            Console.WriteLine("A)neftci XD");
+            Console.WriteLine("B)QARABAG");
+            Console.WriteLine("C)sabah :D");
+
+            string cavab1 = Console.ReadLine();
+
+
+            if (cavab1=="b")
+
+            {
+                Console.WriteLine("BELI DOGRUDUR!!");
+            }
+
+            else
+            {
+                Console.WriteLine("sehvdi,neftci ve sabah qazdi");
+            }
+
+
+
+
+
+            string cavab2 = Console.ReadLine();*/
+            
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
